9c4e193ee9df3f427512342d9bfc0b2054a66c76
Jae No <jaeyno@gmail.com>
2021-07-03 22:31:25 -0400

    [Discount API] migrate the postresql database

--- Microservices/Discount.API/obj/Debug/net5.0/Discount.API.AssemblyInfo.cs
+++ Microservices/Discount.API/obj/Debug/net5.0/Discount.API.AssemblyInfo.cs
@@ -11,6 +11,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("fcaa7dc3-c767-4d44-8ced-c5ae3b1ea093")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("Discount.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
