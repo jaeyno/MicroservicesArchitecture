7d7eee580d1d1ee02dbf40557fc021594cc5bbdb
Jae No <jaeyno@gmail.com>
2021-07-05 10:16:37 -0400

    [Discount gRPC] configured the setting to use the namespace, "Discount.Grpc.Protos"

---
+++ Microservices/Discount.API/Discount.Grpc/Services/DiscountService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Discount.Grpc.Protos;
+
+namespace Discount.Grpc.Services
+{
+    public class DiscountService : DiscountProtoService.DiscountProtoServiceBase
+    {
+        public DiscountService()
+        {
+        }
+    }
+}
